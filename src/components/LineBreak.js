355c0fb717189e326728800254f7c33bcf6878e5
Moody Rahman <moody.byte@gmail.com>
2022-02-01 20:55:42 -0500

    extracted horizonral linebreaks into a component

---
+++ src/components/LineBreak.js
@@ -0,0 +1,17 @@
+
+
+const LineBreak = () => {
+    return (
+        <hr
+        style={{
+          marginTop: "75px",
+          marginBottom: "75px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "50%",
+        }}
+      />
+    )
+}
+
+export default LineBreak
